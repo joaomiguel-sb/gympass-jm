e92301485431e8f0c5eabadbc84e98b9582e8034
João Miguel <80652132+joaomiguel-sb@users.noreply.github.com>
2025-03-12 19:16:42 -0300

    Update index.ts

--- src/env/index.ts
+++ src/env/index.ts
@@ -7,7 +7,7 @@ import "dotenv/config";
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   JWT_SECRET: z.string(),
-  PORT: z.coerce.number().default(3000),
+  PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
